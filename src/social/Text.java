29e754e411251e1e47a6e4098893bc062ccad016
Chrisl187 <herzingerchristian@gmail.com>
2024-01-29 12:40:20 +0100

    initial commit

---
+++ src/social/Text.java
@@ -0,0 +1,23 @@
+package social;
+
+import java.util.ArrayList;
+
+public class Text {
+	
+	private ArrayList<Character> text;
+
+	public Text(String text) {
+		writeText(text);
+	}	
+	
+	private void writeText(String s) {
+		for(int i = 0; i < 3;i++) {
+			
+		}
+	}
+	
+	
+	
+	
+
+}
