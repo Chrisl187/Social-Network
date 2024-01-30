0594a348beb742d73ca7a11d633365d9b9073b58
Chrisl187 <herzingerchristian@gmail.com>
2024-01-30 10:29:51 +0100

    Methode-headers

--- src/social/NewsFeed.java
+++ src/social/NewsFeed.java
@@ -16,13 +16,45 @@ public class NewsFeed {
 		messages.add(message);
 	}
 	
-	public void showByUser(String user) {
+	private ArrayList<Submission> submissionPerUser(String user) {
 		ArrayList<Submission> submissions = new ArrayList<>();
 		for (Submission s: messages) {
 			if(s.getAuthor()==user) {
 				submissions.add(s);
 			}
 		}
+		return submissions;
 	}
+	
+	public ArrayList<Submission> picturePerUser(String user) {
+		ArrayList<Submission> submissions = new ArrayList<>();
+		for (Submission s: messages) {
+			if(s.getAuthor()==user) {
+				if(s.getClass().toString() == "social.Text") {
+					submissions.add(s);
+				}
+			}
+		}
+		return submissions;
+	}
+
+	@Override
+	public void printAllSubmission() {
+		
+	}
+	
+	
+	public void printUserSubmission(String user) {
+		ArrayList<Submission> list = new ArrayList<>();
+		list = submissionPerUser(user);
+		
+		
+	}
+	
+	
+	public void deleteSubmission(int pos) {
+		
+	}
+	
 
 }
--- src/social/Text.java
+++ src/social/Text.java
@@ -4,19 +4,15 @@ import java.util.ArrayList;
 
 public class Text extends Submission{
 
-	private ArrayList<String> text;
+	private String text;
 
 	
 
-	public Text(String author, long timestamp, int likes, ArrayList<String> comments, ArrayList<String> text) {
+	public Text(String author, long timestamp, int likes, ArrayList<String> comments, String text) {
 		super(author, timestamp, likes, comments);
 		this.text = text;
 	}
 
-	private void writeText(String s) {
-		for (int i = 0; i < s.length(); i++) {
-			text.add(s.substring(i, i + 1));
-		}
-	}
+	
 
 }
