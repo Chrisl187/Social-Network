04c040e9792a5ea158ca4fcd9116aedcd58259b7
Chrisl187 <herzingerchristian@gmail.com>
2024-01-30 11:12:29 +0100

    Prints

--- src/social/NewsFeed.java
+++ src/social/NewsFeed.java
@@ -3,58 +3,66 @@ package social;
 import java.util.ArrayList;
 
 public class NewsFeed {
-	
+
 	private ArrayList<Submission> messages;
 
-	
 	public NewsFeed(Submission message) {
 		super();
 		addSubmission(message);
 	}
-	
+
 	private void addSubmission(Submission message) {
 		messages.add(message);
 	}
-	
+
 	private ArrayList<Submission> submissionPerUser(String user) {
 		ArrayList<Submission> submissions = new ArrayList<>();
-		for (Submission s: messages) {
-			if(s.getAuthor()==user) {
+		for (Submission s : messages) {
+			if (s.getAuthor() == user) {
 				submissions.add(s);
 			}
 		}
 		return submissions;
 	}
-	
+
 	public ArrayList<Submission> picturePerUser(String user) {
 		ArrayList<Submission> submissions = new ArrayList<>();
-		for (Submission s: messages) {
-			if(s.getAuthor()==user) {
-				if(s.getClass().toString() == "social.Text") {
+		for (Submission s : messages) {
+			if (s.getAuthor() == user) {
+				if (s.getClass().toString() == "social.Text") {
 					submissions.add(s);
 				}
 			}
 		}
 		return submissions;
 	}
-
-	@Override
-	public void printAllSubmission() {
 		
+	
+	public void printAllSubmissions() {
+		for (Submission s : messages) {
+			if(s.getClass().toString() == "social.Text") {
+				System.out.println("Author: " + s.getAuthor() + "\nTimestamp: " + s.getTimestamp() + "\n Text: " + ((Text) s).getText() + "\nLikes: " +  s.getLikes() + "\nComments: " + s.getComments() + "\n");
+			}else {
+				System.out.println("Author: " + s.getAuthor() + "\nTimestamp: " + s.getTimestamp() + "\n Title: " + ((Picture) s).getTitle() + "\n Picture: " + ((Picture) s).getFilename()  + "\nLikes: " +  s.getLikes() + "\nComments: " + s.getComments() + "\n");
+			}
+		}
 	}
-	
-	
+
 	public void printUserSubmission(String user) {
 		ArrayList<Submission> list = new ArrayList<>();
 		list = submissionPerUser(user);
-		
-		
+		for (Submission s : list) {
+			if(s.getClass().toString() == "social.Text") {
+				System.out.println("Author: " + s.getAuthor() + "\nTimestamp: " + s.getTimestamp() + "\n Text: " + ((Text) s).getText() + "\nLikes: " +  s.getLikes() + "\nComments: " + s.getComments() + "\n");
+			}else {
+				System.out.println("Author: " + s.getAuthor() + "\nTimestamp: " + s.getTimestamp() + "\n Title: " + ((Picture) s).getTitle() + "\n Picture: " + ((Picture) s).getFilename()  + "\nLikes: " +  s.getLikes() + "\nComments: " + s.getComments() + "\n");
+			}
+		}
+
 	}
-	
-	
+
 	public void deleteSubmission(int pos) {
-		
+
 	}
-	
 
 }
--- src/social/Text.java
+++ src/social/Text.java
@@ -2,17 +2,21 @@ package social;
 
 import java.util.ArrayList;
 
-public class Text extends Submission{
+public class Text extends Submission {
 
 	private String text;
-
-	
 
 	public Text(String author, long timestamp, int likes, ArrayList<String> comments, String text) {
 		super(author, timestamp, likes, comments);
 		this.text = text;
 	}
 
-	
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
 
 }
